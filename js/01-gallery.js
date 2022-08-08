258a5e01d5a9821658f650ef32ded837bb3b4f63
Denis <fox.ruden@gmail.com>
2022-08-09 01:18:49 +0300

    theend

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// Реализация делегирования на div.gallery и получение url большого изображения.
+// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+
+// Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе <img>, и указываться в href ссылки. Не добавляй другие HTML теги или CSS классы кроме тех, что есть в этом шаблоне.
+
+const divGallery = document.querySelector(".gallery");
+
+galleryItems.map((item) => {
+ divGallery.insertAdjacentHTML(
+  "afterbegin",
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+   <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-fox="${item.original}"
+    alt="${item.description}"
+   />
+  </a>
+ </div>`
+ );
+});
+
+divGallery.addEventListener("click", noGalleryClick);
+
+function noGalleryClick(eve) {
+ eve.preventDefault();
+ document.addEventListener("keydown", onListenerEscapeKeydown);
+ const srcBigImages = eve.target.dataset.fox;
+ const bigImges = basicLightbox.create(`
+    <img src="${srcBigImages}" >
+`);
+   
+   bigImges.show();
+   
+ if (eve.target.nodeName !== "IMG") return;
+
+ function onListenerEscapeKeydown(event) {
+  console.log(event.key);
+  if (event.key === "Escape") bigImges.close();
+
+  document.removeEventListener("keydown", onListenerEscapeKeydown);
+ }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// Сделай такую же галерею как в первом задании, но используя библиотеку SimpleLightbox, которая возьмет на себя обработку кликов по изображениям, открытие и закрытие модального окна, а также пролистывание изображений при помощи клавиатуры. Посмотри демо видео работы галереи с подключенной библиотекой.
+// Необходимо немного изменить разметку карточки галереи, используй этот шаблон.
 
-console.log(galleryItems);
+// <a class="gallery__item" href="large-image.jpg">
+//   <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+// </a>
+
+const divGallery = document.querySelector(".gallery");
+galleryItems.map((item) => {
+ divGallery.insertAdjacentHTML(
+  "afterbegin",
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+   <img
+    class="gallery__image"
+    src="${item.preview}"
+    data-fox="${item.original}"
+    alt="${item.description}"
+   />
+  </a>
+ </div>`
+ );
+});
+let lightbox = new SimpleLightbox(".gallery a", {
+ /* options */
+ captionPosition: "bottom",
+captionDelay: 250,
+ captionsData: "alt",
+});
+
+// и добавь отображение подписей к изображениям из атрибута alt. Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+
+
